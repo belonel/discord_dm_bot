8313c0465dcaf8b1d6fbf33d26c5e0da33e2866e
belonel <ksandr.lr@gmail.com>
2021-01-21 23:16:20 +0300

    Discord bot first run

---
+++ dmbot.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import discord
+
+from boto.s3.connection import S3Connection
+token = S3Connection(os.environ['D_TOKEN'])
+
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def DM(ctx, user: discord.User, *, message=None):
+    message = message or "This Message is sent via DM"
+    await user.send(message)
+
+bot.run(token)
