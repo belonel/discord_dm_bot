4eee57cf3fbf6610a6665ae5a10e5f2d319297c8
belonel <ksandr.lr@gmail.com>
2021-01-27 12:54:51 +0300

    just token and boto3

--- dmbot.py
+++ dmbot.py
@@ -1,16 +1,18 @@
 from discord.ext import commands
 import discord
+from boto.s3.connection import S3Connection
 import os
 
-from boto.s3.connection import S3Connection
-# token = S3Connection(os.environ['D'])
+token = S3Connection(os.environ['D_KEY'], os.environ['A_Knt(EY'])
+print(token)
+print(type(token))
 
 
-bot = commands.Bot(command_prefix='!')
+# bot = commands.Bot(command_prefix='!')
 
-@bot.command()
-async def DM(ctx, user: discord.User, *, message=None):
-    message = message or "This Message is sent via DM"
-    await user.send(message)
+# @bot.command()
+# async def DM(ctx, user: discord.User, *, message=None):
+#     message = message or "This Message is sent via DM"
+#     await user.send(message)
 
-bot.run('token')
+# bot.run('token')
