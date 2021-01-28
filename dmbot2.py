9374e2863b827acc1c7436c1e657bc7cffa17531
belonel <ksandr.lr@gmail.com>
2021-01-28 12:19:17 +0300

    Update dmbot2.py

--- dmbot2.py
+++ dmbot2.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     # print_all_users()
     Process(target=server_run).start()
     # server_run()
-    # Process(target=bot_run).start()
+    Process(target=bot_run).start()
