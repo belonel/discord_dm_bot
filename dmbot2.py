1e422a3a07dff6af19df95e693bf0e25939e3c46
belonel <ksandr.lr@gmail.com>
2021-02-05 18:32:15 +0300

    Update dmbot2.py

--- dmbot2.py
+++ dmbot2.py
@@ -9,8 +9,8 @@ def bot_run():
 
 def server_run():
     print("Listening...")
-    app.run(debug=False, host='0.0.0.0', port=8085)
-    # port = int(os.environ.get("PORT", 5000))
+    # app.run(debug=False, host='0.0.0.0', port=8085)
+    port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=port)
 def server_alive():
     while(True):
