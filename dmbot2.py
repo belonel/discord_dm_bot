78594112f483ce8cd0c9b21915b01fdd02469c38
belonel <ksandr.lr@gmail.com>
2021-02-08 22:57:15 +0300

    without i'm alive print

--- dmbot2.py
+++ dmbot2.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process
+from threading import Thread
 from bot_logic import *
 from config import *
 from server_logic import *
@@ -12,17 +13,11 @@ def server_run():
     # app.run(debug=False, host='0.0.0.0', port=8085)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-def server_alive():
-    while(True):
-        print("*server alive*")
-        time.sleep(5)
 
 if __name__ == '__main__':
-    # print('hello')
+    print('hello')
     # print_all_users()
     Process(target=server_run).start()
-    time.sleep(1)
     # server_run()
     # bot_run()
     Process(target=bot_run).start()
-    Process(target=server_alive).start()
