82faa15a458ceb58b9752df2a8226dedde8189ca
belonel <ksandr.lr@gmail.com>
2021-02-10 19:48:56 +0300

    only return 200 status check

--- server_logic.py
+++ server_logic.py
@@ -73,56 +73,56 @@ def charge_handle():
 
     data = request.json
 
-    if data['email'] != None and data['stripe_cust_id'] != None and data['customer_name'] != None \
-            and data['product_name'] != None and data['subs_price'] != None and data['subs_interval'] != None \
-            and data['payment_status'] != None and data['failure_message'] != None:
-        event_args = {}
-        if data['payment_status'] == 'succeeded':
-            event_args = {
-                "user_id": str(data['email']),
-                "event_type": "Success payment",
-                "event_properties": {
-                    "product_name": data['product_name'],
-                    "subscription_interval": data['subs_interval'],
-                },
-                "price": float(data['subs_price']),
-                "quantity": 1,
-                "revenue": float(data['subs_price']),
-                "productId": data['product_name'],
-                "revenueType": "Success Charge",
-                "user_properties": {
-                    "customer_name": data['customer_name'],
-                }
-            }
+    #     if data['email'] != None and data['stripe_cust_id'] != None and data['customer_name'] != None \
+    #             and data['product_name'] != None and data['subs_price'] != None and data['subs_interval'] != None \
+    #             and data['payment_status'] != None and data['failure_message'] != None:
+    #         event_args = {}
+    #         if data['payment_status'] == 'succeeded':
+    #             event_args = {
+    #                 "user_id": str(data['email']),
+    #                 "event_type": "Success payment",
+    #                 "event_properties": {
+    #                     "product_name": data['product_name'],
+    #                     "subscription_interval": data['subs_interval'],
+    #                 },
+    #                 "price": float(data['subs_price']),
+    #                 "quantity": 1,
+    #                 "revenue": float(data['subs_price']),
+    #                 "productId": data['product_name'],
+    #                 "revenueType": "Success Charge",
+    #                 "user_properties": {
+    #                     "customer_name": data['customer_name'],
+    #                 }
+    #             }
 
-            indentify_args = {
-                "user_id": str(data['email']),
-                "user_properties": {
-                    "$add": {
-                        "#payments": 1,
-                        "total_revenue_from_user": float(data['subs_price']),
-                    },
-                }
-            }
-            identify = amplitude_logger.create_ident(**indentify_args)
-            amplitude_logger.log_ident(identify)
-        elif data['payment_status'] == 'failed':
-            event_args = {
-                "user_id": str(data['email']),
-                "event_type": "Failed Charge",
-                "event_properties": {
-                    "reason": data['failure_message'],
-                    "product_name": data['product_name'],
-                },
-                "price": -float(data['subs_price']),
-                "revenue": -float(data['subs_price']),
-                "productId": data['product_name'],
-                "revenueType": "Refund",
-            }
+    #             indentify_args = {
+    #                 "user_id": str(data['email']),
+    #                 "user_properties": {
+    #                     "$add": {
+    #                         "#payments": 1,
+    #                         "total_revenue_from_user": float(data['subs_price']),
+    #                     },
+    #                 }
+    #             }
+    #             identify = amplitude_logger.create_ident(**indentify_args)
+    #             amplitude_logger.log_ident(identify)
+    #         elif data['payment_status'] == 'failed':
+    #             event_args = {
+    #                 "user_id": str(data['email']),
+    #                 "event_type": "Failed Charge",
+    #                 "event_properties": {
+    #                     "reason": data['failure_message'],
+    #                     "product_name": data['product_name'],
+    #                 },
+    #                 "price": -float(data['subs_price']),
+    #                 "revenue": -float(data['subs_price']),
+    #                 "productId": data['product_name'],
+    #                 "revenueType": "Refund",
+    #             }
 
-        event = amplitude_logger.create_event(**event_args)
-        # send event to amplitude
-        amplitude_logger.log_event(event)
+    #         event = amplitude_logger.create_event(**event_args)
+    #         # send event to amplitude
+    #         amplitude_logger.log_event(event)
 
     return jsonify({'status': 'ok'}), 200
 
