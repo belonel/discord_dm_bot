8fcd372716bf5f08e06689842b2cd11a0f71986c
belonel <ksandr.lr@gmail.com>
2021-04-29 15:16:14 +0300

    Super Forecasters trial added

--- server_logic.py
+++ server_logic.py
@@ -9,6 +9,42 @@ import asyncio
 
 app = Flask(__name__)
 
+def send_trial_event_to_MS_amplitude(data):
+    event_args = {
+        "user_id": str(data['email']),
+        "event_type": "Trial started",
+        "event_properties": {
+            "product_name": data['product_name'],
+            "subscription_price": data['subs_price'],
+            "subscription_interval": data['subs_interval'],
+        },
+        "user_properties": {
+            "discord_invite_code": data['invite_code'],
+            "stripe_cust_id": data['stripe_cust_id'],
+        }
+    }
+    event = amplitude_logger.create_event(**event_args)
+    # send event to amplitude
+    amplitude_logger.log_event(event)
+
+    indentify_args = {
+        "user_id": str(data['email']),
+        "user_properties": {
+            "$setOnce": {
+                "cohort_day_number": datetime.now().timetuple().tm_yday,
+                "cohort_week_number": date.today().isocalendar()[1],
+                "cohort_month": datetime.now().timetuple().tm_mon,
+                "cohort_year": datetime.now().timetuple().tm_year,
+                "trial_started_at": datetime.now().isoformat(),
+            },
+            "$append": {
+                "subscribed_to": data['product_name']
+            }
+        }
+    }
+    identify = amplitude_logger.create_ident(**indentify_args)
+    amplitude_logger.log_ident(identify)
+
 @app.route('/trial', methods=['POST'])
 def handle_push():
     print(request.data.decode('utf-8'))
@@ -20,41 +56,13 @@ def handle_push():
     if data['email'] != None and data['stripe_cust_id'] != None \
             and data['created_at'] != None and data['invite_code'] != None \
             and data['product_name'] != None and data['subs_price'] != None and data['subs_interval'] != None:
-        insert_user(data['email'], data['stripe_cust_id'], data['created_at'], data['invite_code'])
-        event_args = {
-            "user_id": str(data['email']),
-            "event_type": "Trial started",
-            "event_properties": {
-                "product_name": data['product_name'],
-                "subscription_price": data['subs_price'],
-                "subscription_interval": data['subs_interval'],
-            },
-            "user_properties": {
-                "discord_invite_code": data['invite_code'],
-                "stripe_cust_id": data['stripe_cust_id'],
-            }
-        }
-        event = amplitude_logger.create_event(**event_args)
-        # send event to amplitude
-        amplitude_logger.log_event(event)
 
-        indentify_args = {
-            "user_id": str(data['email']),
-            "user_properties": {
-                "$setOnce": {
-                    "cohort_day_number": datetime.now().timetuple().tm_yday,
-                    "cohort_week_number": date.today().isocalendar()[1],
-                    "cohort_month": datetime.now().timetuple().tm_mon,
-                    "cohort_year": datetime.now().timetuple().tm_year,
-                    "trial_started_at": datetime.now().isoformat(),
-                },
-                "$append": {
-                    "subscribed_to": data['product_name']
-                }
-            }
-        }
-        identify = amplitude_logger.create_ident(**indentify_args)
-        amplitude_logger.log_ident(identify)
+        if 'MS' in data['product_name']:
+            send_trial_event_to_MS_amplitude(data)
+            insert_user(data['email'], data['stripe_cust_id'], data['created_at'], data['invite_code'], 'Macro Sentiments')
+        elif 'SF' in data['product_name']:
+            insert_user(data['email'], data['stripe_cust_id'], data['created_at'], data['invite_code'],
+                        'Super Forecasters')
 
     return jsonify({'status': 'ok'}), 200
 
@@ -75,7 +83,7 @@ def charge_handle():
 
     if data['email'] != None and data['stripe_cust_id'] != None and data['customer_name'] != None \
             and data['product_name'] != None and data['subs_price'] != None and data['subs_interval'] != None \
-            and data['payment_status'] != None and data['failure_message'] != None: 
+            and data['payment_status'] != None and data['failure_message'] != None:
         if data['payment_status'] == 'succeeded':
             print('succeeded')
             event_args = {
@@ -84,7 +92,6 @@ def charge_handle():
                 "event_properties": {
                     "product_name": data['product_name'],
                     "subscription_interval": data['subs_interval'],
-                    "price": float(data['subs_price']),
                 },
                 "price": float(data['subs_price']),
                 "quantity": 1,
@@ -117,7 +124,6 @@ def charge_handle():
                 "event_properties": {
                     "reason": data['failure_message'],
                     "product_name": data['product_name'],
-                    "price": float(data['subs_price']),
                 },
                 "price": 0,
                 "revenue": 0,
@@ -147,7 +153,6 @@ def refund_handle():
             "event_properties": {
                 "reason": data['reason'],
                 "product_name": data['product_name'],
-                "amount_refunded": float(data['amount']),
             },
             "price": -float(data['amount']),
             "revenue": -float(data['amount']),
